83f0f2199fd1f60e0003ed2e5ce7053d5ffc5a99
nishant hind <nishanthind@gmail.com>
2024-08-23 13:11:25 +0530

    this is second commit

--- InsertEx.java
+++ InsertEx.java
@@ -1,9 +1,9 @@
 // Program in Java to insert a string in Stringbuffer object using insert().
-class InsertEx{
-  public static void main(String args[]){
+class InsertEx {
+  public static void main(String args[]) {
     StringBuffer sb = new StringBuffer("Ee");
     System.out.println("Buffer before insert: " + sb);
-    sb.insert(4,"duW");
-    System.out.println("Buffer after insert: " + sb);
+    sb.insert(4, "duW");
+    System.out.println("The Buffer after insert: " + sb);
   }
 }
